1a5c43feea236318f87b629eaf4f779504c2861e
kubofac <kubofac@gmail.com>
2026-05-19 21:53:23 +0900

    Update circuit_data.js

--- circuit_data.js
+++ circuit_data.js
@@ -1,10 +1,26 @@
-// circuit_data.js
+// circuit_data.js　セクター対応
 const circuitData = {
     "0": {
         name: "白糸S",
         startLineP1: [138.622532514261, 35.3081896115975],
         startLineP2: [138.622637774426, 35.3080490509764]
     },
+
+// セクター1
+        s1LineP1: [138.621000, 35.307500], 
+        s1LineP2: [138.621100, 35.307400],
+        
+        // セクター2 (このように増やします)
+        s2LineP1: [138.623000, 35.309000], 
+        s2LineP2: [138.623100, 35.308900],
+        
+        // セクター3 (このように増やします)
+        s3LineP1: [138.624000, 35.310000], 
+        s3LineP2: [138.624100, 35.309900]
+    },
+
+
+    
     "1": {
         name: "富士C",
         startLineP1: [138.93342054530245, 35.36915226810625],
@@ -22,19 +38,29 @@ const circuitData = {
     }
 };
 
-function loadCircuitStartLineFromData(circuitId) {
-    if (circuitData[circuitId]) {
-        const data = circuitData[circuitId];
-        document.getElementById('startLineP1Lon').value = data.startLineP1[0];
-        document.getElementById('startLineP1Lat').value = data.startLineP1[1];
-        document.getElementById('startLineP2Lon').value = data.startLineP2[0];
-        document.getElementById('startLineP2Lat').value = data.startLineP2[1];
-        return { p1: data.startLineP1, p2: data.startLineP2 };
-    } else {
-        document.getElementById('startLineP1Lon').value = '';
-        document.getElementById('startLineP1Lat').value = '';
-        document.getElementById('startLineP2Lon').value = '';
-        document.getElementById('startLineP2Lat').value = '';
-        return { p1: [0, 0], p2: [0, 0] };
-    }
+// HTML側の設計（localStorage + drawSavedLines）に合わせた読み込み関数
+function loadCircuitLinesToStorage(circuitId) {
+    if (!circuitData[circuitId]) return null;
+    
+    const data = circuitData[circuitId];
+    const targets = ['startLine', 's1Line', 's2Line', 's3Line'];
+    
+    targets.forEach(t => {
+        // 例: startLineP1 や s1LineP1 というキーが存在するかチェック
+        if (data[t + 'P1'] && data[t + 'P2']) {
+            // [Lng, Lat] の順で格納されているため、[1]がLat、[0]がLon
+            localStorage.setItem(t + 'P1Lat', data[t + 'P1'][1]);
+            localStorage.setItem(t + 'P1Lon', data[t + 'P1'][0]);
+            localStorage.setItem(t + 'P2Lat', data[t + 'P2'][1]);
+            localStorage.setItem(t + 'P2Lon', data[t + 'P2'][0]);
+        } else {
+            // プリセットにそのセクターが無い場合は古い設定を消去
+            localStorage.removeItem(t + 'P1Lat');
+            localStorage.removeItem(t + 'P1Lon');
+            localStorage.removeItem(t + 'P2Lat');
+            localStorage.removeItem(t + 'P2Lon');
+        }
+    });
+    
+    return data;
 }
