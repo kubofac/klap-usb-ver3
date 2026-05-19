e89d24dd707e7172a157b004a37709f56ee43f0a
kubofac <kubofac@gmail.com>
2026-05-19 22:04:07 +0900

    Update circuit_data.js

--- circuit_data.js
+++ circuit_data.js
@@ -1,26 +1,20 @@
-// circuit_data.js　セクター対応
+// circuit_data.js セクター対応版
 const circuitData = {
     "0": {
         name: "白糸S",
         startLineP1: [138.622532514261, 35.3081896115975],
-        startLineP2: [138.622637774426, 35.3080490509764]
-  
-
-// セクター1
+        startLineP2: [138.622637774426, 35.3080490509764],
+        // セクター1
         s1LineP1: [138.621000, 35.307500], 
         s1LineP2: [138.621100, 35.307400],
-        
-        // セクター2 (このように増やします)
+        // セクター2
         s2LineP1: [138.623000, 35.309000], 
         s2LineP2: [138.623100, 35.308900],
-        
-        // セクター3 (このように増やします)
+        // セクター3
         s3LineP1: [138.624000, 35.310000], 
         s3LineP2: [138.624100, 35.309900]
-    },
+    }, // 白糸Sのデータはここで初めて閉じます
 
-
-    
     "1": {
         name: "富士C",
         startLineP1: [138.93342054530245, 35.36915226810625],
@@ -46,15 +40,12 @@ function loadCircuitLinesToStorage(circuitId) {
     const targets = ['startLine', 's1Line', 's2Line', 's3Line'];
     
     targets.forEach(t => {
-        // 例: startLineP1 や s1LineP1 というキーが存在するかチェック
         if (data[t + 'P1'] && data[t + 'P2']) {
-            // [Lng, Lat] の順で格納されているため、[1]がLat、[0]がLon
             localStorage.setItem(t + 'P1Lat', data[t + 'P1'][1]);
             localStorage.setItem(t + 'P1Lon', data[t + 'P1'][0]);
             localStorage.setItem(t + 'P2Lat', data[t + 'P2'][1]);
             localStorage.setItem(t + 'P2Lon', data[t + 'P2'][0]);
         } else {
-            // プリセットにそのセクターが無い場合は古い設定を消去
             localStorage.removeItem(t + 'P1Lat');
             localStorage.removeItem(t + 'P1Lon');
             localStorage.removeItem(t + 'P2Lat');
